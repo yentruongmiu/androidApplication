8942c664424e654d9a3fb30d5dd5dadd8c34d6b7
Yen Truong <truthblue82@gmail.com>
2023-04-05 01:16:15 -0500

    set random background color for mainlayout, ImageButton in app

--- app/src/main/java/miu/edu/backgroundcolorchanger/MainActivity.kt
+++ app/src/main/java/miu/edu/backgroundcolorchanger/MainActivity.kt
@@ -1,11 +1,28 @@
 package miu.edu.backgroundcolorchanger
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import miu.edu.backgroundcolorchanger.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    internal lateinit var layout: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.button.setOnClickListener{
+            var color = (Math.random() * 16777215).toInt() or (0xFF shl 24)
+            layout = findViewById(R.id.mainlayout)
+            layout.setBackgroundColor(color)
+        }
+        binding.button2.setOnClickListener{
+            var color = (Math.random() * 16777215).toInt() or (0xFF shl 24)
+            it.setBackgroundColor(color)
+        }
     }
+
 }
